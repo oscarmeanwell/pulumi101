1b9be48c8ea06a5d02b5583c19b7cbc27d7b86d1
Oscar Meanwell <oscar@gearedapp.co.uk>
2021-09-20 22:13:20 +0100

    Work in progress

--- infrastructure/api-gateway/index.ts
+++ infrastructure/api-gateway/index.ts
@@ -1,52 +1,30 @@
-// export const apiGateway = new awsx.apigateway.API(
-//     getResourceName('internalRestAPI'),
-//     {
-//       stageName: pulumi.getStack(),
-//       stageArgs: {
-//         accessLogSettings: {
-//           destinationArn: internalRestAPILogGroup.arn,
-//           format: JSON.stringify({
-//             requestId: '$context.requestId',
-//             ip: '$context.identity.sourceIp',
-//             caller: '$context.identity.caller',
-//             user: '$context.identity.user',
-//             requestTime: '$context.requestTime',
-//             httpMethod: '$context.httpMethod',
-//             resourcePath: '$context.resourcePath',
-//             status: '$context.status',
-//             protocol: '$context.protocol',
-//             responseLength: '$context.responseLength',
-//           }),
-//         },
-//         tags: getResourceTags(
-//           'internal-rest-api',
-//           'API Gateway for Stamp Free internal REST API',
-//         ),
-//       },
-  
-//       routes: [
+import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
+import * as pulumi from '@pulumi/pulumi'
 
-//         // Catch all OPTIONS requests and return generic headers
-//         // Adapted from https://www.youtube.com/watch?v=typ-AJQGKKI
-//         // Solves CORS issues encountered when calling from local or remote server
-//         {
-//           path: '/{proxy+}',
-//           method: 'OPTIONS',
-//           eventHandler: async () => {
-//             return {
-//               body: '',
-//               statusCode: 200,
-//               headers: {
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Access-Control-Allow-Credentials': 'true',
-//                 'Access-Control-Allow-Methods':
-//                   'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-//                 'Access-Control-Allow-Headers':
-//                   'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//               },
-//             }
-//           },
-//         },
-//       ],
-//     },
-//   )
+export const apiGateway = new awsx.apigateway.API(
+    'rest-api',
+    {
+      stageName: pulumi.getStack(),
+      routes: [
+        {
+          path: '/{proxy+}',
+          method: 'OPTIONS',
+          eventHandler: async () => {
+            return {
+              body: '',
+              statusCode: 200,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Methods':
+                  'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+                'Access-Control-Allow-Headers':
+                  'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+              },
+            }
+          },
+        },
+      ],
+    },
+  )
