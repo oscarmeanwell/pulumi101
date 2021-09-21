def029e5bea9de76d54765388ce35c00cb8aad4d
Oscar Meanwell <oscar@gearedapp.co.uk>
2021-09-21 20:46:43 +0100

    Run prettier

--- infrastructure/api-gateway/routes/whisky/index.ts
+++ infrastructure/api-gateway/routes/whisky/index.ts
@@ -1,12 +1,11 @@
 import { listWhiskysLambda } from "../../../lambdas/whisky/list-whisky";
-import * as awsx from '@pulumi/awsx'
+import * as awsx from "@pulumi/awsx";
 
 export const apiGatewayWhiskys: awsx.apigateway.Route[] = [
-    {
-      path: '/whiskys',
-      method: 'GET',
-      eventHandler: listWhiskysLambda ,
-      //authorizers: [endUserAuthoriser],
-    },
-  ]
-  
+  {
+    path: "/whiskys",
+    method: "GET",
+    eventHandler: listWhiskysLambda,
+    //authorizers: [endUserAuthoriser],
+  },
+];
--- infrastructure/api-gateway/routes/wine/index.ts
+++ infrastructure/api-gateway/routes/wine/index.ts
@@ -1,15 +1,14 @@
-import * as awsx from '@pulumi/awsx'
+import * as awsx from "@pulumi/awsx";
 import { listWinesLambda } from "../../../lambdas/wine/list-wine";
 
 export const apiGatewayWines: awsx.apigateway.Route[] = [
-    {
-      path: '/wines',
-      method: 'GET',
-      eventHandler: listWinesLambda ,
-      //authorizers: [endUserAuthoriser],
-    },
-  ]
-  
+  {
+    path: "/wines",
+    method: "GET",
+    eventHandler: listWinesLambda,
+    //authorizers: [endUserAuthoriser],
+  },
+];
 
 /** Authoriser for end users */
 // export const endUserAuthoriser = awsx.apigateway.getCognitoAuthorizer({
